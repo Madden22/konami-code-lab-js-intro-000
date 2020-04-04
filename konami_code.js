09e6ab75f98138615a727abe8fe5a4d20d7ba431
Sydney Michael <m13s19madden@gmail.com>
2020-04-04 16:43:18 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,36 @@ const codes = [
 
 function init() {
   // your code here
-}
+    var idx = 0;
+    //keys_arr = []
+    document.addEventListener('keydown', function(e) {
+    //console.log(e.key);
+    const key = e.key;
+
+    if (key === codes[idx]) {
+      idx++
+      //console.log('yep')
+
+      if (idx === codes.length) {
+        console.log('reached')
+        alert('Congrats! You cracked the Konami Code!!');
+        idx = 0;
+      }
+    } else {
+      idx = 0;
+    }
+    /*var chker = keys_arr.slice(keys_arr.length -10, keys_arr.length)
+    function chkTr (ar1, ar2) {
+      for (let i = 0; i < ar1.length; i++) {
+        if (ar1[i] != ar2[i]) {
+          return false
+        } else {
+          return true
+        }
+      }
+    }
+    if (chkTr(codes, chker)) {
+      alert('Congrats! You cracked the Konami Code!!')
+    }
+})*/
+})}
